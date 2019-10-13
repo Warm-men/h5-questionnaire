b009cc8e508396773e16a24a30f6428234d4034e
Neigevoir <neigevoir@NeigevoirdeMacBook-Pro.local>
2019-10-13 21:12:09 +0800

    add swiper

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -10,49 +10,49 @@ const browserHistory = createBrowserHistory()
 
 class App extends React.PureComponent {
   componentDidMount() {
-    const search = parseQueryString(window.location.search)
-    // 新用户还没有登录过
-    if (!storage.get('refresh_token', localStorage)) {
-      if (_.isEmpty(search.code)) {
-        wxLogin()
-      } else {
-        ajaxJsonp({
-          url: '/api/account/wchatH5Login',
-          isNotParams: true,
-          data: {
-            code: search.code
-          },
-          success: res => {
-            if (res.code === 1) {
-              const { openid, nickname, headimgurl } = res.data
-              const data = {
-                open_id: openid,
-                user_name: nickname,
-                head_img: headimgurl,
-                login_type: 1,
-                app_type: global.app_type
-              }
-              ajaxJsonp({
-                url: '/api/Account/login',
-                data,
-                success: loginRes => {
-                  const { refresh_token, token } = loginRes.data
-                  storage.set('refresh_token', refresh_token, localStorage)
-                  storage.set('token', token, localStorage)
-                  this.forceUpdate()
-                }
-              })
-            }
-          }
-        })
-      }
-    } else {
-      console.log('登录过！')
-    }
+    // const search = parseQueryString(window.location.search)
+    // // 新用户还没有登录过
+    // if (!storage.get('refresh_token', localStorage)) {
+    //   if (_.isEmpty(search.code)) {
+    //     wxLogin()
+    //   } else {
+    //     ajaxJsonp({
+    //       url: '/api/account/wchatH5Login',
+    //       isNotParams: true,
+    //       data: {
+    //         code: search.code
+    //       },
+    //       success: res => {
+    //         if (res.code === 1) {
+    //           const { openid, nickname, headimgurl } = res.data
+    //           const data = {
+    //             open_id: openid,
+    //             user_name: nickname,
+    //             head_img: headimgurl,
+    //             login_type: 1,
+    //             app_type: global.app_type
+    //           }
+    //           ajaxJsonp({
+    //             url: '/api/Account/login',
+    //             data,
+    //             success: loginRes => {
+    //               const { refresh_token, token } = loginRes.data
+    //               storage.set('refresh_token', refresh_token, localStorage)
+    //               storage.set('token', token, localStorage)
+    //               this.forceUpdate()
+    //             }
+    //           })
+    //         }
+    //       }
+    //     })
+    //   }
+    // } else {
+    //   console.log('登录过！')
+    // }
   }
 
   render() {
-    if (storage.get('refresh_token', localStorage)) {
+    if (!storage.get('refresh_token', localStorage)) {
       return (
         <Router history={browserHistory}>
           <Routers />
