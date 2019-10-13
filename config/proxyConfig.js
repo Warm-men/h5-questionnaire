cf28648df650a2dbcd9e1da6119772781efd989f
Hodor-chen <hodor.chen@letote.cn>
2019-10-13 21:33:33 +0800

    修改接口url

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   proxy: {
     '/api': {
-      target: 'https://activity.yypiano.cn',
+      target: 'http://backend.zhihui92.cn',
       secure: false,
       changeOrigin: true,
       preserveHeaderKeyCase: true,
