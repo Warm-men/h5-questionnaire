2889f573e64647d4f8aa6464765e96de9587504c
Hodor-chen <hodor.chen@letote.cn>
2019-10-14 23:02:00 +0800

    添加第一第二屏动画

--- src/containers/swiper_component/component/second_page/index.js
+++ src/containers/swiper_component/component/second_page/index.js
@@ -1,7 +1,28 @@
 import './index.scss'
 
-class SecondPage extends React.Component {
+class SecondPage extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isShowIntroduction: false
+    }
+  }
+  _showInroduction = () => this.setState({ isShowIntroduction: true })
+  _hideInroduction = () => this.setState({ isShowIntroduction: false })
   render() {
+    const { currentPageIndex } = this.props
+    const onFocus = currentPageIndex === 1
+    const titleStyle = onFocus ? 'jackInTheBox title-image' : 'title-image'
+    const bottomButtonStyle = onFocus
+      ? 'bottom-button-image slideInDown'
+      : 'bottom-button-image'
+    const dataListStyle = onFocus ? 'jello data-list-image' : 'data-list-image'
+    const menStyle = onFocus ? 'men-image shake' : 'men-image'
+    const introduteIconStyle = onFocus
+      ? 'introdute-icon-image fadeIn'
+      : 'introdute-icon-image'
+    const mouseStyle = onFocus ? 'mouse-image fadeIn' : 'mouse-image'
+    const womenStyle = onFocus ? 'women-image slideInLeft' : 'mouse-image'
     return (
       <div className="second-page-container">
         <img
@@ -9,25 +30,25 @@ class SecondPage extends React.Component {
           alt=""
           className="bg-image"
         />
-        <div className="title-image">
+        <div className={titleStyle}>
           <img src={require('../first_page/images/title.png')} alt="" />
         </div>
-        <div className="data-list-image">
+        <div className={dataListStyle}>
           <img src={require('./images/page2_data_list.png')} alt="" />
         </div>
         <div className="coin-image">
           <img src={require('../first_page/images/page1_coin.png')} alt="" />
         </div>
-        <div className="mouse-image">
+        <div onClick={this._showInroduction} className={mouseStyle}>
           <img src={require('./images/page2_mouse_icon.png')} alt="" />
         </div>
-        <div className="introdute-icon-image">
+        <div onClick={this._showInroduction} className={introduteIconStyle}>
           <img src={require('./images/page2_introdute_icon.png')} alt="" />
         </div>
-        <div className="men-image">
+        <div className={menStyle}>
           <img src={require('./images/page2_men.png')} alt="" />
         </div>
-        <div className="women-image">
+        <div className={womenStyle}>
           <img src={require('./images/page2_women.png')} alt="" />
         </div>
         <div className="fade-image">
@@ -36,9 +57,17 @@ class SecondPage extends React.Component {
         <div className="plant-image">
           <img src={require('../first_page/images/plant.png')} alt="" />
         </div>
-        <div className="bottom-button-image">
+        <div className={bottomButtonStyle}>
           <img src={require('./images/page2_bottom_button.png')} alt="" />
         </div>
+        {this.state.isShowIntroduction ? (
+          <div className="pop-wrapper-view">
+            <div className={'introduction-image fadeIn'}>
+              <div className="hide-button" onClick={this._hideInroduction} />
+              <img src={require('./images/page2_introdution_pop.png')} alt="" />
+            </div>
+          </div>
+        ) : null}
       </div>
     )
   }
--- src/containers/swiper_component/index.jsx
+++ src/containers/swiper_component/index.jsx
@@ -4,29 +4,48 @@ import Swiper from 'react-id-swiper'
 import './index.scss'
 import 'react-id-swiper/lib/styles/css/swiper.css'
 import { First, SecondPage, ThirdPage } from './component'
-export default function IndexContainer(props) {
-  const options = {
-    direction: 'vertical'
+export default class IndexContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentPageIndex: 0
+    }
+    this.options = {
+      direction: 'vertical',
+      on: {
+        slideNextTransitionEnd: this._slideNextTransitionEnd,
+        slidePrevTransitionEnd: this._slidePrevTransitionEnd
+      }
+    }
+  }
+  _slideNextTransitionEnd = () => {
+    this.setState({ currentPageIndex: this.state.currentPageIndex + 1 })
   }
 
-  return (
-    <div className="home-container">
-      <Helmet title="问卷" link="index" />
-      <Swiper
-        slideClass="custom-swiper-slide"
-        wrapperClass="custom-swiper-wrapper"
-        {...options}
-      >
-        <div className="swiper-image">
-          <First />
-        </div>
-        <div className="swiper-image">
-          <SecondPage />
-        </div>
-        <div className="swiper-image">
-          <ThirdPage />
-        </div>
-      </Swiper>
-    </div>
-  )
+  _slidePrevTransitionEnd = () =>
+    this.setState({ currentPageIndex: this.state.currentPageIndex - 1 })
+
+  render() {
+    const { currentPageIndex } = this.state
+    return (
+      <div className="home-container">
+        <Helmet title="问卷" link="index" />
+        <Swiper
+          slideClass="custom-swiper-slide"
+          wrapperClass="custom-swiper-wrapper"
+          {...this.options}
+        >
+          <div className="swiper-image">
+            <First currentPageIndex={currentPageIndex} />
+          </div>
+          <div className="swiper-image">
+            <SecondPage currentPageIndex={currentPageIndex} />
+          </div>
+          <div className="swiper-image">
+            <ThirdPage currentPageIndex={currentPageIndex} />
+          </div>
+        </Swiper>
+      </div>
+    )
+  }
 }
