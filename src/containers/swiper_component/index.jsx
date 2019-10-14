a06e5c30ba8e72a5affb222e2fd9b3e452855f24
Neigevoir <neigevoir@NeigevoirdeMacBook-Pro.local>
2019-10-14 09:47:15 +0800

    fix styles

--- src/containers/swiper_component/index.jsx
+++ src/containers/swiper_component/index.jsx
@@ -2,45 +2,31 @@ import Helmet from 'src/lib/pagehelmet.js'
 import wxInit from 'src/lib/wx_config.js'
 import Swiper from 'react-id-swiper'
 import './index.scss'
-import 'swiper/css/swiper.min.css'
 import 'react-id-swiper/lib/styles/css/swiper.css'
 import { First, SecondPage, ThirdPage } from './component'
-class IndexContainer extends React.Component {
-  constructor(props) {
-    super()
-    this.options = {
-      direction: 'vertical'
-    }
+export default function IndexContainer(props) {
+  const options = {
+    direction: 'vertical'
   }
 
-  componentDidMount() {}
-
-  next = () => {
-    this.props.history.push('/introduce')
-  }
-
-  render() {
-    return (
-      <div className="home-container">
-        <Helmet title="问卷" link="index" />
-        <Swiper
-          slideClass="custom-swiper-slide"
-          wrapperClass="custom-swiper-wrapper"
-          {...this.options}
-        >
-          <div className="swiper-image">
-            <First />
-          </div>
-          <div className="swiper-image">
-            <SecondPage />
-          </div>
-          <div className="swiper-image">
-            <ThirdPage />
-          </div>
-        </Swiper>
-      </div>
-    )
-  }
+  return (
+    <div className="home-container">
+      <Helmet title="问卷" link="index" />
+      <Swiper
+        slideClass="custom-swiper-slide"
+        wrapperClass="custom-swiper-wrapper"
+        {...options}
+      >
+        <div className="swiper-image">
+          <First />
+        </div>
+        <div className="swiper-image">
+          <SecondPage />
+        </div>
+        <div className="swiper-image">
+          <ThirdPage />
+        </div>
+      </Swiper>
+    </div>
+  )
 }
-
-export default IndexContainer
